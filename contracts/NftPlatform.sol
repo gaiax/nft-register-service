2f964510dcca5e9c0d0114b617cf3d31301854ac
Jo-Shino <joe000042@gmail.com>
2022-10-12 14:25:08 +0900

    metadataの設定とHexStrings.solの設定

--- contracts/NftPlatform.sol
+++ contracts/NftPlatform.sol
@@ -6,13 +6,25 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "base64-sol/base64.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import './libraries/HexStrings.sol';
 
 contract NftPlatform is ERC721, AccessControl{
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIdCounter; //最新のトークンIDが入ります
+  using HexStrings for uint256;
 
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
   bytes32 public constant REGISTER_ROLE = keccak256("REGISTER_ROLE");
+
+  mapping(uint256 => Product) public products;
+
+  struct Product {
+    string name;
+    string image;
+    string description;
+    uint16 price;
+    address seller;
+  }
 
   constructor(address _register, address _minter) ERC721("NftPlatform", "NPF") {
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -20,11 +32,25 @@ contract NftPlatform is ERC721, AccessControl{
     _setupRole(MINTER_ROLE, _minter);
   }
 
-  function safeMint(address to)
+  function safeMint(
+    string memory _name,
+    string memory _image,
+    string memory _description,
+    uint16 _price,
+    address _seller,
+    address to
+  )
   public 
   onlyRole(MINTER_ROLE)
   {
     _safeMint(to, _tokenIdCounter.current());
+    products[_tokenIdCounter.current()] = Product({
+      name: _name,
+      image: _image,
+      description: _description,
+      price: _price,
+      seller: _seller
+    });
     _tokenIdCounter.increment();
   }
 
@@ -41,13 +67,21 @@ contract NftPlatform is ERC721, AccessControl{
         Base64.encode(
           bytes(
             abi.encodePacked(
-              "{\"name\":\"namae\",\"description\":\"", tokenId,"\"}"
+              "{\"name\":\"", products[tokenId].name,
+              "\",\"image\":\"", products[tokenId].image,
+              "\",\"description\":\"", products[tokenId].description,
+              "\",\"price\":\"", Strings.toString(products[tokenId].price),
+              "\",\"seller\":\"", addressToString(products[tokenId].seller),
+              "\"}}"
             )
           )
         )
       )
     );
+  }
 
+  function addressToString(address addr) internal pure returns (string memory) {
+    return  (uint256(uint160(addr))).toHexString(20);
   }
 
   function supportsInterface(bytes4 interfaceId)
