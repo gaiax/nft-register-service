87b99d1ad8c4b1551e531463deb00f97bcf8fcb0
Jo-Shino <joe000042@gmail.com>
2022-10-12 11:45:59 +0900

    TokenURI関数の雛形作成

--- contracts/NftPlatform.sol
+++ contracts/NftPlatform.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "base64-sol/base64.sol";
 
 contract NftPlatform is ERC721, AccessControl{
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
@@ -13,7 +14,29 @@ contract NftPlatform is ERC721, AccessControl{
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _setupRole(REGISTER_ROLE, _register);
     _setupRole(MINTER_ROLE, _minter);
-    }
+  }
+
+  function tokenURI(uint256 tokenId) 
+  public 
+  view
+  override(ERC721)
+  returns (string memory)
+  {
+    return 
+    string (
+      abi.encodePacked(
+        "data:application/json;base64,",
+        Base64.encode(
+          bytes(
+            abi.encodePacked(
+              "{\"name\":\"namae\",\"description\":\"setumei\"}"
+            )
+          )
+        )
+      )
+    );
+
+  }
 
   function supportsInterface(bytes4 interfaceId)
     public
