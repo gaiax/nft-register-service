a5b7c4242bed97a4052f1a7b85beb41aec029c13
Jo-Shino <joe000042@gmail.com>
2022-10-15 10:22:58 +0900

    safeMint関数が動くまでテストした

--- scripts/run.ts
+++ scripts/run.ts
@@ -2,9 +2,20 @@ import { ethers } from "hardhat";
 const main = async () => {
 	const [deployer] = await ethers.getSigners();
 	const nftplatform = await ethers.getContractFactory("NftPlatform");
-	const network = await nftplatform.deploy(deployer.address, deployer.address);
-	await network.deployed();
-	console.log("The NftPlatform NFT deployed to:", network.address);
+	const nftContract = await nftplatform.deploy(deployer.address, deployer.address);
+	await nftContract.deployed();
+	console.log("The NftPlatform NFT deployed to:", nftContract.address);
+  const mint = await nftContract.safeMint(
+    "0xe4832791325a4519E96881e9798DBf1e88Ed6724",
+    "name",
+    "https://i.imgur.com/6Qb13Z4.jpeg",
+    "description",
+    "1000",
+    "0xe4832791325a4519E96881e9798DBf1e88Ed6724"
+  );
+  await mint.wait();
+  const tokenURI = await nftContract.tokenURI(0);
+  console.log(tokenURI);
 };
 
 main().catch((error) => {
