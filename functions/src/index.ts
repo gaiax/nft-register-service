ea97ca48b94705f901db65f2fd3e0e109cccba61
Jo-Shino <joe000042@gmail.com>
2022-10-21 17:23:59 +0900

    ethersをfunctions下にインストール

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,7 +17,6 @@ expressApp.get("/helthCheck", express.json(), async (request: any, response: any
   });
 })
 
-
 expressApp.post("/mint", express.json(), async (request: any, response: any) => {
   response.set({ 'Access-Control-Allow-Origin': '*' });
 
@@ -35,12 +34,21 @@ expressApp.post("/mint", express.json(), async (request: any, response: any) => 
   );
 
   const mint = await Nftplatform.safeMint(
-    signer.address,
-    request.body.name, 
-    request.body.image, 
-    request.body.description,
-    request.body.price,
-    request.body.seller
+    "0xe4832791325a4519E96881e9798DBf1e88Ed6724",
+    // "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+    "name",
+    "image",
+    "description",
+    "1000",
+    "0xe4832791325a4519E96881e9798DBf1e88Ed6724"
+    // "0xBcd4042DE499D14e55001CcbB24a551F3b954096"
+
+    // signer.address,
+    // request.body.name, 
+    // request.body.image, 
+    // request.body.description,
+    // request.body.price,
+    // request.body.seller
   );
   await mint.wait();
   
