3b4c362ae5d75abcc49c6824146e3dd24ec70937
Jo-Shino <joe000042@gmail.com>
2022-10-21 17:48:12 +0900

    testnetとmainnetのdeply用にdeploy.tsを調整した

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,16 @@
+require("dotenv").config();
 import { ethers } from "hardhat";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
 
+  const registerAddress = String(process.env.REGISTER_ADDRESS);
+  const minterAddress = String(process.env.MINTER_ADDRESS);
+  console.log(registerAddress);
+  console.log(minterAddress);
+
   const NftPlatform = await ethers.getContractFactory("NftPlatform");
-  const nftPlatform = await NftPlatform.deploy(deployer.address, deployer.address);
+  const nftPlatform = await NftPlatform.deploy(registerAddress, minterAddress);
 
   await nftPlatform.deployed();
   console.log("nftPlatform deployed to:" , nftPlatform.address);
