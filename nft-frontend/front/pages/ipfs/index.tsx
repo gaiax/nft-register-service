eb28c518dbfda3b8645b892578895f6dcdb4de1f
Jo-Shino <joe000042@gmail.com>
2022-10-26 10:56:59 +0900

    IPFSにアップロード完了

--- nft-frontend/front/pages/ipfs/index.tsx
+++ nft-frontend/front/pages/ipfs/index.tsx
@@ -1,29 +1,7 @@
 import React, { useState } from "react"
 import Layout from '../../components/layouts/index'
 import { ReactElement } from 'react';
-
-// const env = require.main.require('./env');
-
-// //後ほどipfsをインストールする
-// const IPFS = require('ipfs-http-client')
-
-//後ほどIDとKEYを設定する
-//読み込めていないのでenvファイルの読み込み方を確認する
-const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID;
-const INFURA_SECRET_KEY = process.env.NEXT_PUBLIC_INFURA_SECRET_KEY;
-
-// const auth =
-//   "Basic " + Buffer.from(INFURA_ID + ":" + INFURA_SECRET_KEY).toString("base64");
-
-// const ipfs = new IPFS({
-//   host: "ipfs.infura.io",
-//   port: "5001",
-//   protocol: "https",
-//   headers: {
-//     authorization: auth,
-//   },
-//   timeout: 100000,
-// });
+import { imageToIpfsHash } from '../../components/ipfs'
 
 
 export default function Ipfs() {
@@ -42,36 +20,37 @@ export default function Ipfs() {
       console.log(reader.result);
     };
     console.log(buffer);
-    console.log(INFURA_ID);
   };
 
-  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   console.log('submitted...');
-  //   setLoad(false);
-  //   const hash = await getImageIpfsHash(buffer);
-  //   setResultHash(hash);
-  //   console.log(hash);
-  //   setEnd(true);
-  // }
-  // const getImageIpfsHash = async (imageData: any) => {
-  //   const base64 = imageData.substring(imageData.indexOf(',') + 1)
-  //   const binary_string = window.atob(base64)
-  //   const binary_length = binary_string.length
-  //   const bytes = new Uint8Array(binary_length)
-  //   for (let i = 0; i < binary_length; i++) {
-  //     bytes[i] = binary_string.charCodeAt(i)
-  //   }
-  //   const ipfsHash = await ipfs.add(bytes.buffer)
-  //   return ipfsHash.path
-  // }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('submitted...');
+    setLoad(false);
+    const hash = await imageToIpfsHash(buffer);
+    setResultHash(hash);
+    console.log(hash);
+    setEnd(true);
+  }
+  
 
   return (
     <div>
-      <form >
+      <form onSubmit={handleSubmit}>
         <input type="file" onChange={handleChange}></input>
         <button>Submit</button>
       </form>
+
+      <div>IPFS Hash : https://ipfs.io/ipfs/{resultHash}</div>
+        <div>
+          IPFS Link is{' '}
+          <a
+            href={`https://ipfs.io/ipfs/${resultHash}`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            here
+          </a>
+        </div>
     </div>
 
   )
