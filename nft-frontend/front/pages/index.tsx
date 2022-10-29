b6fb7f547cc6142803091d5b3313a0403f10e283
Jo-Shino <joe000042@gmail.com>
2022-10-29 16:15:57 +0900

    firestoreのNFTsの全ドキュメント取得完了

--- nft-frontend/front/pages/index.tsx
+++ nft-frontend/front/pages/index.tsx
@@ -2,30 +2,49 @@ import { Component, useEffect, useCallback, useState } from 'react';
 import { useRouter } from 'next/router';
 import { ReactElement } from 'react';
 import Layout from '../components/layouts/index'
-import { NFT } from '../utils/type'
+import MediaCard from '../components/card/Card'
+import { NFT, ServerResponse } from '../utils/type'
 import axios, {AxiosResponse} from "axios"
+import Image from 'next/image'
 
 //後ほどここに現在販売中のリボンを表示するようにする
 
 export default function Home() {
   const [nfts, setNfts] = useState<NFT[]>([])
 
-  const fetchNFTs = useCallback(async () => {
+  const getNfts =  async () => {
     const nftDocs = await axios.get(process.env.NEXT_PUBLIC_API_BASE + '/getNfts')
-      .then((res) => {
-        console.log(res);
-        console.log(res.data.NFTs);
-        setNfts(res.data.NFTs);
-    });
+    .then((res: ServerResponse) =>res.data);
+    console.log(nftDocs);
+    return nftDocs;
+  }
+
+  const fetchNFTs = useCallback(async () => {
+    const newDocs = await getNfts();
+       
+    await setNfts(newDocs.NFTs);
+    console.log(newDocs.NFTs)
   }, [])
 
   useEffect(() => {
     fetchNFTs()
   }, []);
 
+  const TestSample = () => {
+    const TopNFT = nfts[0];
+    console.log(nfts[0].imageURL);
+  }
+
   return (
     <div>
       リダイレクトします。
+
+      <button onClick={TestSample} />
+      {/* <MediaCard img="https://i.imgur.com/AD3MbBi.jpeg"/> */}
+      {/* <img src="https://ipfs.io/ipfs/QmSc6JkFPzkE4XioSLpKaC32Sp1zYZfpQMXkeiuQ6fJMCB"></img> */}
+      {/* <img src="https://i.imgur.com/AD3MbBi.jpeg"></img> */}
+      {/* <Image src={nfts[0].imageURL}></Image> */}
+     
     </div>
   )
 }
